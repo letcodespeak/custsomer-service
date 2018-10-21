a7b000ac1038da0c097f893a35bcb26e205bf3fb
Matt Zhang <m.zhang@intershop.com>
2018-10-21 16:50:54 +1100

    initial commit

---
+++ src/main/java/com/codespeaks/rest/customerservice/customerservice/CustomerserviceApplication.java
@@ -0,0 +1,12 @@
+package com.codespeaks.rest.customerservice.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerserviceApplication.class, args);
+	}
+}
