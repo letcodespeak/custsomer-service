1bf7fcf0a2abcd59d9150928ed1a3d0be16806b1
Matt Zhang <m.zhang@intershop.com>
2018-10-28 21:16:05 +1100

    changes

--- src/main/java/com/codespeaks/rest/service/customerservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codespeaks.rest.service.customerservice.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codespeaks.rest.service.customerservice.domain.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
